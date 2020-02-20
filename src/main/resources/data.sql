90b6eff8c018e6e5672c319d7f40851ef8de8e72
hebel1992 <bartosz.hebrycz@gmail.com>
2020-02-20 16:01:23 +0100

    orders init at the start of server

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,6 +19,17 @@ INSERT INTO employees (address, date_of_birth, first_name, last_name, phone_numb
 INSERT INTO employees (address, date_of_birth, first_name, last_name, phone_number, rate_per_hour) VALUES ('ul. Fioletowa 5, Morawa', '1999-01-06', 'Kamil', 'Gierek', 412123123, 12.0);
 INSERT INTO employees (address, date_of_birth, first_name, last_name, phone_number, rate_per_hour) VALUES ('ul. Krucha 115, Oborniki', '1978-12-11', 'Jaroslaw', 'Polanski', 991000111, 4.0);
 
+INSERT INTO orders (created, initial_diagnosis, planned_repair_start, title, status_id, vehicle_id) VALUES (NOW(), 'Timing gear broken - need to be changed', DATE_ADD(NOW(),INTERVAL +5 DAY), 'Timing gear change', 1, 1);
+INSERT INTO orders_employees (order_id, employee_id) VALUES (1,1);
+INSERT INTO orders (actual_repair_start, created, initial_diagnosis, planned_repair_start, title, status_id, vehicle_id) VALUES (NOW(), DATE_ADD(NOW(),INTERVAL -5 DAY), 'Engine oil need to be changed', NOW(), 'Oil change', 2, 2);
+INSERT INTO orders_employees (order_id, employee_id) VALUES (2,1);
+INSERT INTO orders_employees (order_id, employee_id) VALUES (2,2);
+INSERT INTO orders (actual_repair_start, created, initial_diagnosis, planned_repair_start, title, status_id, vehicle_id) VALUES (DATE_ADD(NOW(),INTERVAL -2 DAY), DATE_ADD(NOW(),INTERVAL -4 DAY), 'Lights aiming too low, need adjustment', DATE_ADD(NOW(),INTERVAL -3 DAY), 'Lights adjustment', 2, 3);
+INSERT INTO orders_employees (order_id, employee_id) VALUES (3,3);
+INSERT INTO orders (actual_repair_start, created, initial_diagnosis, planned_repair_start, title, status_id, vehicle_id, cancel_reason) VALUES (DATE_ADD(NOW(),INTERVAL -1 DAY), DATE_ADD(NOW(),INTERVAL -3 DAY), 'Tyres worn out, need replacement', DATE_ADD(NOW(),INTERVAL -1 DAY), 'Tyres change', 4, 4, 'Client gave up the order');
+INSERT INTO orders_employees (order_id, employee_id) VALUES (4,4);
+
+
 INSERT INTO role (`id`, `name`) VALUES (NULL, 'ROLE_ADMIN');
 INSERT INTO role (`id`, `name`) VALUES (NULL, 'ROLE_USER');
 

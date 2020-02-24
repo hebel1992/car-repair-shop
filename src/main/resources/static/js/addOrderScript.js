1af14a8c5be7500c63785ba535df015a1316b6a0
hebel1992 <bartosz.hebrycz@gmail.com>
2020-02-24 23:14:08 +0100

    adding order form redesigned

--- src/main/resources/static/js/addOrderScript.js
+++ src/main/resources/static/js/addOrderScript.js
@@ -1,5 +1,5 @@
 $(function () {
-    document.getElementById('checkBox').onchange = function () {
+    document.getElementById('check-box').onchange = function () {
         let plannedDate = document.getElementById('plannedDate');
         plannedDate.value = new Date().toISOString().substr(0, 10);
         plannedDate.readOnly = this.checked;

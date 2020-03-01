c1b8ac0622ee41f548da19814681b34dfca9a7d2
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-01 11:17:45 +0100

    comm

--- src/main/java/pl/coderslab/servicestation/WebSecurityConfig.java
+++ src/main/java/pl/coderslab/servicestation/WebSecurityConfig.java
@@ -15,16 +15,6 @@ import pl.coderslab.servicestation.services.SpringDataUserDetailsService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SpringDataUserDetailsService customUserDetailsService() {
-        return new SpringDataUserDetailsService();
-    }
-
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -55,5 +45,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().accessDeniedPage("/access-denied");
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService() {
+        return new SpringDataUserDetailsService();
+    }
 
 }

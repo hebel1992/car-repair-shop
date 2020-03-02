4fee205fa0246470904275453ed0dd26b253e5a2
hebel1992 <bartosz.hebrycz@gmail.com>
2020-03-02 19:56:51 +0100

    vehicle repository query fix

--- src/main/java/pl/coderslab/servicestation/repositories/OrderRepository.java
+++ src/main/java/pl/coderslab/servicestation/repositories/OrderRepository.java
@@ -2,6 +2,7 @@ package pl.coderslab.servicestation.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.coderslab.servicestation.models.Order;
 
 import java.util.List;
@@ -17,8 +18,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findFinishedAndCancelledOrders();
 
     @Query(value = "SELECT o FROM Order o WHERE o.vehicle.id = :vehicleId AND (o.status.id=1 OR o.status.id=2)")
-    List<Order> findCurrentOrdersByVehicleId(Long vehicleId);
+    List<Order> findCurrentOrdersByVehicleId(@Param("vehicleId") Long vehicleId);
 
     @Query(value = "SELECT o FROM Order o WHERE o.vehicle.id = :vehicleId AND (o.status.id=3 OR o.status.id=4)")
-    List<Order> findHistoryOrdersByVehicleId(Long vehicleId);
+    List<Order> findHistoryOrdersByVehicleId(@Param("vehicleId") Long vehicleId);
 }
